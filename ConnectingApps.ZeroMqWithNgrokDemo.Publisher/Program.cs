4cf1f46cabae499d23613e78d3f2e8bb3091fb00
Daan Acohen <daan.acohen@connectingapps.net>
2025-05-06 16:33:37 +0200

    Add two console projects

---
+++ ConnectingApps.ZeroMqWithNgrokDemo.Publisher/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace ConnectingApps.ZeroMqWithNgrokDemo.Publisher;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
---
+++ ConnectingApps.ZeroMqWithNgrokDemo.Subscriber/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace ConnectingApps.ZeroMqWithNgrokDemo.Subscriber;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
